8124a82965f0ed16e27229fd36d707f3eae31f02
William Gu <74311720+stationarybird@users.noreply.github.com>
2024-04-30 15:58:19 +0000

    add club

--- main.py
+++ main.py
@@ -5,13 +5,16 @@ import logging
 import sqlite3
 
 
-values = {"club_name":"", "description":"", "date":"", "categories":"", "e":""}
+values = {"club_name":"", "sponsor":"", "days":"", "time":"", "location":"", "category":"", "e":""}
 
 app = fk.Flask(
     __name__,
     static_folder="stylesheets",
     template_folder="templates",
 )
+
+def escape_html(s):
+  return html.escape(s)
 
 def get_connection():
   connection = sqlite3.connect("blogbosts.db")
@@ -27,7 +30,7 @@ def get_clubs():
 def create_new_club():
   with get_connection() as connection:
     cursor = connection.cursor()
-    cursor.execute("INSERT INTO posts (club_name, description, date, categories) VALUES (?, ?, ?, ?)", (values["club_name"], values["description"], values["date"], values["categories"] ))#stuff should go inside that 
+    cursor.execute("INSERT INTO posts (club_name, sponsor, days, time, location, category) VALUES (?, ?, ?, ?, ?, ?)", (values["club_name"], values["sponsor"], values["days"], values["time"], values["location"], values["category"] ))
     connection.commit()
 
 @app.route('/', methods=["GET"])
@@ -35,12 +38,18 @@ def create_new_club():
 def root():
   with sqlite3.connect("clubs.db") as con:
     cursor = con.cursor()
-    cursor.execute("CREATE TABLE IF NOT EXISTS clubs (name TEXT, description TEXT, date TEXT, categories TEXT)")
+    cursor.execute("CREATE TABLE IF NOT EXISTS clubs (name TEXT, sponsor TEXT, days TEXT, meetingTime TEXT, location TEXT, categories TEXT)")
     cursor.execute("SELECT * FROM clubs")
   return fk.render_template("home.html")
 
 @app.route('/addclub', methods=["GET"])
 def add_club():
+  method = fk.request.method
+  if method=="GET":
+    values['club_name'] = escape_html(fk.request.form['name'])
+    values['sponsor'] = escape_html(fk.request.form['sponsor'])
+    
+    
   return fk.render_template("addclub.html")
 
 
