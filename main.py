e37cdcf4a20f50ba01a3397280174046d3871786
William Gu <74311720+stationarybird@users.noreply.github.com>
2024-04-30 15:18:27 +0000

    working on db

--- main.py
+++ main.py
@@ -13,11 +13,15 @@ app = fk.Flask(
 @app.route('/', methods=["GET"])
 @app.route('/clubs', methods=["GET"],strict_slashes=False)
 def root():
+  with sqlite3.connect("clubs.db") as con:
+    cursor = con.cursor()
+    cursor.execute("CREATE TABLE IF NOT EXISTS clubs (name TEXT, description TEXT, logo TEXT, url TEXT)")
   return fk.render_template("home.html")
 
 @app.route('/addclub', methods=["GET"])
 def add_club():
   return fk.render_template("addclub.html")
+
 
 
 app.run(host='0.0.0.0', port='3000')
