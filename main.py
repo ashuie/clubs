5d24e99ae2f61077d2f53b748fbed29252e99f96
Ashley Feng <147632986+aszhs@users.noreply.github.com>
2024-05-11 00:46:08 +0000

    im so serious don't change or comment out anything that i just changed

--- main.py
+++ main.py
@@ -5,13 +5,16 @@ import logging
 import html
 import sqlite3
 from flask_wtf import Form, FlaskForm
-from wtforms import (StringField, TextAreaField, IntegerField, BooleanField, RadioField, SelectField, SelectMultipleField)
+from wtforms import (StringField, TextAreaField,  SelectField, SelectMultipleField)
 from wtforms import widgets
 from wtforms.validators import InputRequired, Length, StopValidation
 
 logging.basicConfig(level=logging.DEBUG)
 global db_file
-db_file = "clubs.db"
+db_file = "clubs/clubs.db"
+
+weekdays = { 1 : 'Monday', 2 :'Tuesday', 3 : 'Wednesday', 4 :'Thursday'}
+tags_list = {1 : 'Academic', 2 : 'Arts & Humanities', 3 : 'Competition-based', 4 : 'Community Service', 5 : 'Cultural', 6 : 'Gaming', 7 : 'Leadership', 8 : 'Science & Technology', 9 :  'Sports'}
 
 def get_tags():
   return [(1, 'Academic'), (2, 'Arts & Humanities'), (3, 'Competition-based'), (4, 'Community Service'), (5, 'Cultural'), (6, 'Gaming'), (7, 'Leadership'), (8, 'Science & Technology'), (9, 'Sports')]
@@ -38,7 +41,7 @@ validators=[InputRequired(message = "Please enter a sponsor name")])
                           choices = [(1, 'Monday'), (2, 'Tuesday'), (3, 'Wednesday'), (4, 'Thursday')], 
                           validators=[MultiCheckboxAtLeastOne(message="Please choose at least one day")], coerce=int)
   time = SelectField('Meeting Times', 
-                   choices=[('BeforeSchool', 'Before School'),('Lunch', 'Lunch'), ('AfterSchool', 'After School'), ('Other','Other')],  
+                   choices=[('Before School', 'Before School'),('Lunch', 'Lunch'), ('After School', 'After School'), ('Other','Other')],  
                    validators=[InputRequired("Please select a time")])
   location = StringField('Meeting Location',
 validators=[InputRequired(message = "Please enter a location")])
@@ -60,22 +63,38 @@ app.config["WTF_CSRF_ENABLED"] = False
 def escape_html(s):
   return html.escape(s)
 
+def dict_factory(cursor, row):
+  d = {}
+  for index, col in enumerate(cursor.description):
+      d[col[0]] = row[index]
+  return d
+
 def get_connection():
-  connection = sqlite3.connect("clubs.db")
+  connection = sqlite3.connect("clubs/clubs.db")
+  connection.row_factory = dict_factory
   return connection
 
 def get_clubs():
-  connection = get_connection()
-  cursor = connection.cursor()
-  cursor.execute("SELECT * FROM clubs")
-  clubs = cursor.fetchall()
-  return clubs 
+  with get_connection() as con:
+    cursor = con.cursor()
+    clubs = cursor.execute("SELECT * FROM clubs")
+    logging.info(clubs)
+    return clubs 
 
+def string_convert(dict, list):
+  s = ""
+  for i in range(len(list) - 1):
+    s += dict[list[i]] + ", "
+  s += dict[list[len(list)-1]]
+  return s
+  
 def create_new_club(form):
   with get_connection() as connection:
     cursor = connection.cursor()
     # ADD TO SQL WITH WTFORM
-    cursor.execute("INSERT INTO clubs (name, sponsor, days, time, location, category, contact, description) VALUES (?, ?, ?, ?, ?, ?, ?, ?)", (str(form.name), str(form.sponsor), str(form.days), str(form.time), str(form.location), str(form.tags), str(form.contact), str(form.description)))
+    logging.info(form.tags.data)
+    logging.info(string_convert(tags_list, form.tags.data))
+    cursor.execute("INSERT INTO clubs (name, sponsor, days, time, location, category, contact, description) VALUES (?, ?, ?, ?, ?, ?, ?, ?)", (str(form.name.data), str(form.sponsor.data), string_convert(weekdays, form.days.data), str(form.time.data), str(form.location.data), string_convert(tags_list, form.tags.data), str(form.contact.data), str(form.description.data)))
     connection.commit()
 
 def load_clubs(clubs):
@@ -84,11 +103,11 @@ def load_clubs(clubs):
 @app.route('/', methods=["GET"])
 @app.route('/clubs', methods=["GET"],strict_slashes=False)
 def root():
-  with sqlite3.connect("clubs.db") as con:
+  with sqlite3.connect("clubs/clubs.db") as con:
     cursor = con.cursor()
     cursor.execute("CREATE TABLE IF NOT EXISTS clubs (name TEXT, sponsor TEXT, days TEXT, time TEXT, location TEXT, category TEXT, contact TEXT, description TEXT)")
     clubs = cursor.execute("SELECT * FROM clubs")
-  return load_clubs(get_clubs())
+    return load_clubs(get_clubs())
 
 @app.route('/addclub', methods=["GET", "POST"])
 def add_club():
